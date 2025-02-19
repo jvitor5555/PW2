b1d7a87ac0fb6a5be2dc49d35de2c12dfe4523f6
João Vitor Felismino <153321526+jvitor5555@users.noreply.github.com>
2025-02-19 12:30:02 -0300

    Em casa

---
+++ Code/server.js
@@ -0,0 +1,13 @@
+const dotenv = require('dotenv').config()
+
+const app = require('./app');
+
+const PORT = process.env.PORT|| 8080;
+
+app.listen(PORT, () => {
+    console.log(`
+        Server is listening on port ${PORT}
+        `)
+    console.log(`
+        ${process.env.MSG}`)
+});
